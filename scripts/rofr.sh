80235206cdb26b6927f09e35179ce310887d99cc
Frede Hundewadt <22748698+fhdk@users.noreply.github.com>
2020-10-11 11:00:31 +0200

    fix font in rofr.sh

--- scripts/rofr.sh
+++ scripts/rofr.sh
@@ -14,16 +14,19 @@ Usage:   $Name [options]
 Options:
      -h      Display this message
      -v      Display script version
+     -q      Persistant calculator dialog
      -w      Switch between open windows
      -r      Program launcher & run dialog
-     -m      Launcher click
+     -m      Manjaro click
+     -c      Select previous clipboard entries
+     -b      Browser search by keyword
      -l      Session logout choice
 
 EOF
 }
 
 #  Handle command line arguments
-while getopts ":hvqwcmrl" opt; do
+while getopts ":hvqwcbmrl" opt; do
     case $opt in
         h)
             _usage
@@ -33,9 +36,26 @@ while getopts ":hvqwcmrl" opt; do
             echo -e "$Name -- Version $Version"
             exit 0
             ;;
+        q)
+            rofi -modi "calc:qalc +u8 -nocurrencies" -padding 50 \
+                -show "calc:qalc +u8 -nocurrencies" -line-padding 4 \
+                -hide-scrollbar
+            ;;
         w)
             rofi -modi window -show window -hide-scrollbar \
                 -eh 1 -padding 50 -line-padding 4
+            ;;
+        c)
+            rofi -modi "clipboard:greenclip print" -padding 50 \
+                -line-padding 4 -show "clipboard:greenclip print" \
+                -hide-scrollbar
+            ;;
+        b)
+            surfraw -browser="$BROWSER" $(sr -elvi | awk -F'-' '{print $1}' \
+                | sed '/:/d' | awk '{$1=$1};1' | rofi -hide-scrollbar \
+                -kb-row-select 'Tab' -kb-row-tab 'Control+space' \
+                -dmenu -mesg 'Tab for Autocomplete' -i -p 'Web Search: ' \
+                -padding 50 -line-padding 4)
             ;;
         m)
             rofi -location 1 -yoffset 40 -xoffset 10 \
@@ -49,7 +69,7 @@ while getopts ":hvqwcmrl" opt; do
                 -show-icons -drun-icon-theme "$ICON_THEME"
             ;;
         l)
-            ANS=$(echo "  Lock|  Logout| ⏻ Reboot| ⏼ Shutdown" | \
+            ANS=$(echo " Lock| Logout| Reboot| Shutdown" | \
                 rofi -sep "|" -dmenu -i -p 'System ' "" -width 20 \
                 -hide-scrollbar -eh 1 -line-padding 4 -padding 50 -lines 4)
             case "$ANS" in
